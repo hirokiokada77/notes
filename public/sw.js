221c912c3498d11a4b10ee11bcd7dedbf6e9c6bb
Hiroki Okada <hirokiokada77@gmail.com>
2025-07-03 22:03:20 +0900

    Add empty sw.js to prevent SecurityError in development mode

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+// This empty file is placed here to prevent a SecurityError caused by
+// fetching the nonexistent sw.js in development mode. The service
+// worker is configured to run only in production mode.
