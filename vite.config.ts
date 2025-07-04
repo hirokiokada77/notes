8bd4ab1156137968a5042519fcf36024a0c6ad52
Hiroki Okada <hirokiokada77@gmail.com>
2025-07-04 23:25:19 +0900

    Implement hash-based strict Content Security Policy (CSP)
    Closes #4.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import process from "node:process";
+import { createHash } from "node:crypto";
+import { readFileSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import * as cheerio from "cheerio";
+import type { OutputAsset } from "rollup";
+import { defineConfig, type ResolvedConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import sri from "vite-plugin-sri";
 import { app_description, app_name } from "./src/locales/en.json";
 
 export default defineConfig({
@@ -22,7 +25,6 @@ export default defineConfig({
 				theme_color: "#ffffff",
 			},
 		}),
-		sri(),
 		{
 			name: "inject-app-name",
 			transformIndexHtml(html) {
@@ -35,28 +37,98 @@ export default defineConfig({
 				return html.replace("__app_description__", app_description);
 			},
 		},
-		{
-			name: "inject-csp",
-			transformIndexHtml(html) {
-				return html.replace(
-					"__csp__",
-					process.env.NODE_ENV === "production"
-						? `<meta http-equiv="Content-Security-Policy" content="` +
+		(() => {
+			let config: ResolvedConfig;
+
+			return {
+				name: "inject-csp-and-sri",
+				configResolved(resolvedConfig) {
+					config = resolvedConfig;
+				},
+				writeBundle(options, bundle) {
+					const scriptHashMap = new Map();
+					const stylesheetHashMap = new Map();
+
+					const htmls = Object.keys(bundle)
+						.filter((fileName) => fileName.endsWith(".html"))
+						.filter((fileName) => bundle[fileName].type === "asset")
+						.map((fileName) => bundle[fileName]) as OutputAsset[];
+
+					for (const html of htmls) {
+						const $ = cheerio.load(html.source as string);
+
+						const scripts = $("script[src]");
+						const stylesheets = $("link[rel=stylesheet]");
+
+						for (const script of scripts) {
+							const resourcePath = resolve(
+								options.dir ?? "",
+								script.attribs.src.substring(config.base.length),
+							);
+
+							const source = readFileSync(resourcePath);
+
+							const integrity = `sha384-${createHash("sha384")
+								.update(source)
+								.digest()
+								.toString("base64")}`;
+
+							script.attribs.integrity = integrity;
+
+							scriptHashMap.set(script.attribs.src, integrity);
+						}
+
+						for (const stylesheet of stylesheets) {
+							const resourcePath = resolve(
+								options.dir ?? "",
+								stylesheet.attribs.href.substring(config.base.length),
+							);
+
+							const source = readFileSync(resourcePath);
+
+							const integrity = `sha384-${createHash("sha384")
+								.update(source)
+								.digest()
+								.toString("base64")}`;
+
+							stylesheet.attribs.integrity = integrity;
+
+							stylesheetHashMap.set(stylesheet.attribs.href, integrity);
+						}
+
+						const cspElement = $(
+							`<meta http-equiv="Content-Security-Policy" content="` +
 								[
 									["default-src", "'none'"],
 									["img-src", "data:"],
 									["manifest-src", "'self'"],
 									["require-trusted-types-for", "'script'"],
 									["trusted-types", "app-service-worker-policy"],
-									["script-src", "'self'"],
-									["style-src", "'self'"],
+									[
+										"script-src",
+										Array.from(scriptHashMap.values())
+											.map((hash) => `'${hash}'`)
+											.concat("'strict-dynamic'")
+											.join(" "),
+									],
+									[
+										"style-src",
+										Array.from(stylesheetHashMap.values())
+											.map((hash) => `'${hash}'`)
+											.join(" "),
+									],
 								]
 									.map(([key, value]) => `${key} ${value}`)
 									.join("; ") +
-								`;">`
-						: "",
-				);
-			},
-		},
+								`;">`,
+						);
+
+						$("head").append(cspElement);
+
+						writeFileSync(resolve(options.dir ?? "", html.fileName), $.html());
+					}
+				},
+			};
+		})(),
 	],
 });
