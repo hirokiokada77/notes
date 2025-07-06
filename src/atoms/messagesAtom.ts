eee322e23335ebc2163c6f6dd3c5224e5a451ab2
Hiroki Okada <hirokiokada77@gmail.com>
2025-07-06 18:05:30 +0900

    Fix MessageKeys being unresolved in global.d.ts

--- src/atoms/messagesAtom.ts
+++ src/atoms/messagesAtom.ts
@@ -11,30 +11,6 @@ import ru from "../locales/ru.json";
 import zh from "../locales/zh.json";
 import { type Locale, localeAtom } from "./localeAtom";
 
-export type MessageKeys =
-	| "app_name"
-	| "app_description"
-	| "textarea_placeholder"
-	| "current_url_label"
-	| "copy_button"
-	| "copy_success"
-	| "copy_fail"
-	| "share_instruction"
-	| "locale_selector_label"
-	| "dark_mode_label"
-	| "save_button"
-	| "save_success"
-	| "note_loaded_from_browser"
-	| "save_fail"
-	| "clear_button"
-	| "clear_confirm"
-	| "clear_success"
-	| "clear_fail"
-	| "view_button"
-	| "edit_button"
-	| "qr_code_view_summary"
-	| "qr_code_img_alt";
-
 export interface Messages {
 	app_name: string;
 	app_description: string;
