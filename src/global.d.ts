164a926600cf7a7622407c2479beb13135b1e860
Hiroki Okada <hirokiokada77@gmail.com>
2025-07-06 23:41:55 +0900

    Fix MessageKeys being unresolved in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,26 @@
-export {};
+export type MessageKeys =
+	| "app_name"
+	| "app_description"
+	| "textarea_placeholder"
+	| "current_url_label"
+	| "copy_button"
+	| "copy_success"
+	| "copy_fail"
+	| "share_instruction"
+	| "locale_selector_label"
+	| "dark_mode_label"
+	| "save_button"
+	| "save_success"
+	| "note_loaded_from_browser"
+	| "save_fail"
+	| "clear_button"
+	| "clear_confirm"
+	| "clear_success"
+	| "clear_fail"
+	| "view_button"
+	| "edit_button"
+	| "qr_code_view_summary"
+	| "qr_code_img_alt";
 
 declare global {
 	function registerToastMessage(newToastMessageKey: MessageKeys): void;
