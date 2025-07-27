e3abe2f2ff9441f4731c32dfa78027b997c32547
Hiroki Okada <hirokiokada77@gmail.com>
2025-07-27 23:09:55 +0900

    Eliminate use of non-null assertion

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { store } from "./atoms";
 
-// biome-ignore lint/style/noNonNullAssertion: #root is never null
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLDivElement,
+);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
