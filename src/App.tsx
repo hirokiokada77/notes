43bf43fd49f5481861f7ef74f251cf2e76e8d026
Hiroki Okada <hirokiokada77@gmail.com>
2025-08-03 00:33:38 +0900

    Refactor hook calls in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,13 @@
 import "./App.css";
-import { useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toast } from "./components/Toast";
 import { homePath, savedNotesPath } from "./constants";
-import { useAppDispatch, useAppSelector } from "./hooks";
-import { selectLocale } from "./localeSlice";
+import { useInit } from "./hooks";
 import { Home } from "./pages/Home";
 import { SavedNotes } from "./pages/SavedNotes";
-import { selectStringResources } from "./stringResourcesSlice";
-import { selectTheme, updateTheme } from "./themeSlice";
-import { updateTime } from "./timeSlice";
 
 export const App = () => {
-	const dispatch = useAppDispatch();
-	const locale = useAppSelector(selectLocale);
-	const stringResources = useAppSelector(selectStringResources);
-	const theme = useAppSelector(selectTheme);
-
-	useEffect(() => {
-		const description = document.querySelector("meta[name=description]");
-		description?.setAttribute("content", stringResources.appDescription);
-	}, [stringResources.appDescription]);
-
-	useEffect(() => {
-		const html = document.querySelector("html");
-		html?.setAttribute("lang", locale);
-	}, [locale]);
-
-	useEffect(() => {
-		document.body.classList.remove("dark-mode", "light-mode");
-		document.body.classList.add(theme === "dark" ? "dark-mode" : "light-mode");
-	}, [theme]);
-
-	useEffect(() => {
-		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-		const handleThemeChange = (event: MediaQueryListEvent) => {
-			dispatch(updateTheme(event.matches ? "dark" : "light"));
-		};
-
-		mediaQuery.addEventListener("change", handleThemeChange);
-		return () => {
-			mediaQuery.removeEventListener("change", handleThemeChange);
-		};
-	}, [dispatch]);
-
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			dispatch(updateTime());
-		}, 1000);
-
-		return () => {
-			clearInterval(intervalId);
-		};
-	}, [dispatch]);
+	useInit();
 
 	return (
 		<BrowserRouter>
--- src/hooks.ts
+++ src/hooks.ts
@@ -4,7 +4,11 @@ import {
 	useDispatch,
 	useSelector,
 } from "react-redux";
+import { selectLocale } from "./localeSlice";
 import type { AppDispatch, RootState } from "./store";
+import { selectStringResources } from "./stringResourcesSlice";
+import { selectTheme, updateTheme } from "./themeSlice";
+import { updateTime } from "./timeSlice";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
@@ -14,4 +18,66 @@ export const useTitle = (title: string) => {
 	useEffect(() => {
 		document.title = title;
 	}, [title]);
+};
+
+export const useDescription = () => {
+	const stringResources = useAppSelector(selectStringResources);
+
+	useEffect(() => {
+		const description = document.querySelector("meta[name=description]");
+		description?.setAttribute("content", stringResources.appDescription);
+	}, [stringResources.appDescription]);
+};
+
+export const useLocale = () => {
+	const locale = useAppSelector(selectLocale);
+
+	useEffect(() => {
+		const html = document.querySelector("html");
+		html?.setAttribute("lang", locale);
+	}, [locale]);
+};
+
+export const useTheme = () => {
+	const dispatch = useAppDispatch();
+	const theme = useAppSelector(selectTheme);
+
+	useEffect(() => {
+		document.body.classList.remove("dark-mode", "light-mode");
+		document.body.classList.add(theme === "dark" ? "dark-mode" : "light-mode");
+	}, [theme]);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+		const handleThemeChange = (event: MediaQueryListEvent) => {
+			dispatch(updateTheme(event.matches ? "dark" : "light"));
+		};
+
+		mediaQuery.addEventListener("change", handleThemeChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", handleThemeChange);
+		};
+	}, [dispatch]);
+};
+
+export const useTime = () => {
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			dispatch(updateTime());
+		}, 1000);
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, [dispatch]);
+};
+
+export const useInit = () => {
+	useDescription();
+	useLocale();
+	useTheme();
+	useTime();
 };
