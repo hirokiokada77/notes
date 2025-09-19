94ebc4517dea79b58faffabe67597afe6e8b0677
Hiroki Okada <hirokiokada77@gmail.com>
2025-09-19 20:09:43 +0900

    Remove unused value attribute from checkbox element

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -180,7 +180,6 @@ const NoteListItem = ({
 				<div className="note-list-item-checkbox">
 					<input
 						type="checkbox"
-						value={note.id}
 						checked={selected}
 						onChange={handleItemCheckboxChange}
 					/>
